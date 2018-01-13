3167a61301914bc152f94b924b2e48bf861ba436
Yunus M <myounis.ar@live.com>
2018-01-13 15:22:23 +0530

    HAM v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(meta: Meta, title: Title) {
+
+    title.setTitle('Harvard Art Musuem - Home');
+
+    meta.addTags([
+      { name: 'author', content: 'Younis A M' },
+      { name: 'keywords', content: 'Harvard Art Museum, Art Museum' },
+      { name: 'description', content: 'Harvard Art Museum' }
+    ]);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,77 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+import { EmbedVideo } from 'ngx-embed-video';
 
+//Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ImageDetailsComponent } from './components/image-details/image-details.component';
+import { CollectionsComponent } from './components/collections/collections.component';
+import { ExhibitionsComponent } from './components/exhibitions/exhibitions.component';
+import { ExhibitionComponent } from './components/exhibition/exhibition.component';
+import { CollectionComponent } from './components/collection/collection.component';
+import { ImagesComponent } from './components/images/images.component';
+import { ShopComponent } from './components/shop/shop.component';
+
+//Services
+import { HarvardArtMuseumService } from './services/harvardartmuseum.service';
+import { VimeoService } from './services/vimeo.service';
+import { CollectionInfoService } from './services/collectionInfo.service';
+
+// Firebase imports and config
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { CreditsComponent } from './components/credits/credits.component';
+import { PublicationsComponent } from './components/publications/publications.component';
+import { PublicationComponent } from './components/publication/publication.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC69KhKmbPJOhAYWuvB0DBjSz_77Yetrcs",
+  authDomain: "harvardartmuseums-7ef16.firebaseapp.com",
+  databaseURL: "https://harvardartmuseums-7ef16.firebaseio.com",
+  projectId: "harvardartmuseums-7ef16",
+  storageBucket: "",
+  messagingSenderId: "640456681484"
+};
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    HttpModule,
+    EmbedVideo.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ImageDetailsComponent,
+    CollectionsComponent,
+    ExhibitionsComponent,
+    ExhibitionComponent,
+    CollectionComponent,
+    ImagesComponent,
+    ShopComponent,
+    CreditsComponent,
+    PublicationsComponent,
+    PublicationComponent
+  ],
+  providers: [
+    HarvardArtMuseumService,
+    VimeoService,
+    CollectionInfoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
